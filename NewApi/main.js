5124e82c2c87206cf785e9cf110563de9d088bbc
Noah Saint-Louis <saintlouishg@gmail.com>
2025-04-06 23:44:57 -0400

    rick and morty update

--- NewApi/main.js
+++ NewApi/main.js
@@ -1,15 +1,21 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-document.getElementById('getGenreBtn').addEventListener('click', getGenre);
+document.querySelector('#getPop').addEventListener('click', getRandom)
 
-function getGenre(){
-
-fetch("https://binaryjazz.us/wp-json/genrenator/v1/genre/")
-    .then(res => res.json())
-    .then(data => {
-    console.log(data)
-    document.querySelector('h2').innerText = data
-    })
-    .catch(err=> {
-    console.log(`error${err}`)
-    });
-   }
+function getRandom(){
+const randomId = Math.floor(Math.random() * 826) + 1
+fetch(`https://rickandmortyapi.com/api/character/${randomId}`)
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    document.querySelector('h2').innerText = `Name: ${data.name}`;
+    document.querySelector('#species').innerText = `Species: ${data.species}`;
+    document.querySelector('#gender').innerText = `Gender: ${data.gender}`;
+    document.querySelector('#status').innerText = `Status: ${data.status}`;
+    document.querySelector('#origin').innerText = `Origin: ${data.origin.name}`;
+    document.querySelector('#location').innerText = `Location: ${data.location.name}`;
+    document.querySelector('#characterImage').src = data.image;
+})
+.catch(err => {
+    console.log(`Error: ${err}`);
+});
+}
